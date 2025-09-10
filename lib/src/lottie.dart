736cdbe939171496037de230bcc2d52d56ca1b64
TaeBbong <mok05289@korea.ac.kr>
2025-09-10 19:49:11 +0900

    fix: remove unused parameter(lottieSize)

--- lib/src/lottie.dart
+++ lib/src/lottie.dart
@@ -333,13 +333,12 @@ class _State extends State<Lottie> {
         scale: 1.0 / dpr,
         child: CustomPaint(
           painter: TVGCanvas(
-              width: width,
-              height: height,
-              lottieWidth: lottieWidth.toDouble(),
-              lottieHeight: lottieHeight.toDouble(),
-              renderWidth: renderWidth,
-              renderHeight: renderHeight,
-              image: img!),
+            width: width,
+            height: height,
+            renderWidth: renderWidth,
+            renderHeight: renderHeight,
+            image: img!,
+          ),
         ),
       ),
     );
@@ -351,16 +350,11 @@ class TVGCanvas extends CustomPainter {
       {required this.image,
       required this.width,
       required this.height,
-      required this.lottieWidth,
-      required this.lottieHeight,
       required this.renderWidth,
       required this.renderHeight});
 
   double width;
   double height;
-
-  double lottieWidth;
-  double lottieHeight;
 
   double renderWidth;
   double renderHeight;
