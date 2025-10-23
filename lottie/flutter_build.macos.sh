9b2044b2bdce7c294fe6e5c3cc97f39e259a9f6c
조민서 <49663640+JoMinSeo@users.noreply.github.com>
2025-10-24 00:30:20 +0900

    feat: add macOS Universal Binary build support
    - Add cross-compilation files and build script for macOS arm64/x86_64

--- lottie/flutter_build.macos.sh
+++ lottie/flutter_build.macos.sh
@@ -6,8 +6,7 @@ rm -rf build_flutter_macos
 rm -rf build_flutter_aarch64 libthorvg.a
 mkdir build_flutter_aarch64
 
-cd ../thorvg
-meson setup -Db_lto=true -Ddefault_library=static -Dloaders="lottie, png, jpg" -Dthreads=false --cross-file ../thorvg/cross/macos_arm64.txt ../lottie/build_flutter_aarch64
+meson setup -Db_lto=true -Ddefault_library=static -Dloaders="lottie, png, jpg" -Dthreads=false --cross-file ../cross/macos_arm64.txt ../thorvg build_flutter_aarch64
 
 cd ../lottie
 ninja -C build_flutter_aarch64
@@ -15,15 +14,14 @@ ninja -C build_flutter_aarch64
 cp build_flutter_aarch64/src/libthorvg.a libthorvg.a
 rm -rf build_flutter_aarch64/
 
-meson setup -Db_lto=true -Ddefault_library=static --cross-file ../thorvg/cross/macos_arm64.txt build_flutter_aarch64
+meson setup -Db_lto=true -Ddefault_library=static --cross-file ../cross/macos_arm64.txt build_flutter_aarch64
 ninja -C build_flutter_aarch64/
 
 # Build for x86_64 (Intel)
 rm -rf build_flutter_x86_64 libthorvg.a
 mkdir build_flutter_x86_64
 
-cd ../thorvg
-meson setup -Db_lto=true -Ddefault_library=static -Dloaders="lottie, png, jpg" -Dthreads=false --cross-file ../thorvg/cross/macos_x86_64.txt ../lottie/build_flutter_x86_64
+meson setup -Db_lto=true -Ddefault_library=static -Dloaders="lottie, png, jpg" -Dthreads=false --cross-file ../cross/macos_x86_64.txt ../thorvg build_flutter_x86_64
 
 cd ../lottie
 ninja -C build_flutter_x86_64
@@ -31,7 +29,7 @@ ninja -C build_flutter_x86_64
 cp build_flutter_x86_64/src/libthorvg.a libthorvg.a
 rm -rf build_flutter_x86_64/
 
-meson setup -Db_lto=true -Ddefault_library=static --cross-file ../thorvg/cross/macos_x86_64.txt build_flutter_x86_64
+meson setup -Db_lto=true -Ddefault_library=static --cross-file ../cross/macos_x86_64.txt build_flutter_x86_64
 ninja -C build_flutter_x86_64/
 
 rm -rf libthorvg.a
